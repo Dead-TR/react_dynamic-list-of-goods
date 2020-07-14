84f182ce8106ed5ba8c09963aa8a644c942cf812
DTR <t.terry.rice@gmail.com>
2020-07-14 17:44:15 +0300

    Updates

---
+++ precache-manifest.50e4dce388fd03dea768bd94b87e5bbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6b2c3273865c91a8996655ea92acb0c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a845d49bc7badda9e50d",
+    "url": "./static/css/main.0e1462a4.chunk.css"
+  },
+  {
+    "revision": "259efcf119c44bb14090",
+    "url": "./static/js/2.2ded484a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.2ded484a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a845d49bc7badda9e50d",
+    "url": "./static/js/main.4585aaa4.chunk.js"
+  },
+  {
+    "revision": "cc2a5e26d5c4844758e0",
+    "url": "./static/js/runtime-main.beb1984a.js"
+  }
+]);
